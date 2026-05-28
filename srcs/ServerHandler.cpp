22dc299ad1b7c5c2f64313a6d0855ad05051be0e
nathan bodin <nathanbodin006@gmail.com>
2026-05-28 12:04:58 +0200

    feat(server handler): accept connection and register the new fd and new handler instance into the handlers map

--- srcs/ServerHandler.cpp
+++ srcs/ServerHandler.cpp
@@ -6,17 +6,20 @@
 /*   By: nbodin <nbodin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 11:28:53 by nbodin            #+#    #+#             */
-/*   Updated: 2026/05/28 10:59:01 by nbodin           ###   ########lyon.fr   */
+/*   Updated: 2026/05/28 12:02:40 by nbodin           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ServerHandler.hpp"
+#include "ClientHandler.hpp"
 #include <iostream>
 #include <unistd.h>
 #include <stdexcept>
 #include <cerrno>
 #include <cstring>
 #include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 ServerHandler::ServerHandler(EventLoop &loop) :
     _loop(loop) {}
@@ -44,5 +47,23 @@ void    ServerHandler::onError(int fd)
 
 void    ServerHandler::onReadable(const int fd)
 {
-    (void)fd;
+    int connFd = accept4(fd, NULL, NULL, SOCK_NONBLOCK);
+    if (connFd == -1)
+    {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+        {
+            std::cerr << "Warning: no connection pending: " << strerror(errno) << std::endl;
+            return;
+        }
+        else if (errno == ENETDOWN || errno == EPROTO || errno == ENOPROTOOPT ||
+            errno == EHOSTDOWN || errno == ENONET || errno == EHOSTUNREACH ||
+            errno == EOPNOTSUPP || errno == ENETUNREACH)
+        {
+            std::cerr << "Warning: accept error: " << strerror(errno) << std::endl;
+            return;
+        }
+        throw std::runtime_error(std::string("Error: accept failed: ") + strerror(errno));
+    }
+    this->_loop.registerHandler(connFd, new ClientHandler(this->_loop));
+    //add client
 }
